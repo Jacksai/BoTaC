6f0a3062beee83acf43d0ad67b439664d5451bb7
knatalia92 <k.natalia92@gmail.com>
2014-05-26 02:08:19 +0200

    Częściowa walidacja, dodanie kontrolera friends

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ActiveRecord::Base
+  validates :title, presence: true
+  validates :description, length: { maximum: 200, too_long: "%{count} characters is the maximum allowed" }
   belongs_to :author
   has_and_belongs_to_many :tags
-  enum state: [:accepted, :pending]
   has_many :book_comments
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ActiveRecord::Base
+  validates :title, :content, presence: true
   belongs_to :sender ,class_name: "User"
   belongs_to :reciever, class_name: "User"
 end
