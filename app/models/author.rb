0c2bcf0b13299ca0765b03a96146a9ba99ae653b
= <=>
2014-06-03 16:25:10 +0200

    Przy tworzeniu książki widać imię i nazwisko zamiast samego imienia

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,4 +2,9 @@ class Author < ActiveRecord::Base
 	validates :name, :surname, presence: true
     validates :description, length: { maximum: 200, too_long: "%{count} characters is the maximum allowed" }
 	has_many :books
+
+  def full_name
+    return name+" "+surname
+  end
+
 end
