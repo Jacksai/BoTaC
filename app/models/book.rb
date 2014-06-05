61144315c2b11d08c04be92055217a77ef0eb7c8
= <tmkbwr@gmail.com>
2014-06-05 14:08:37 +0200

    mniejsze poprawki

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,4 +5,22 @@ class Book < ActiveRecord::Base
   has_and_belongs_to_many :tags
   has_many :book_comments
   enum state: [:pending, :accepted]
+
+  # Liczenie średniej ocen
+  def avg_grade
+    if book_comments.count > 0
+    sum = 0
+
+    book_comments.each do |comment|
+      sum = sum + comment.grade
+    end
+
+    return sum / book_comments.count
+
+    else
+      return 0
+    end
+
+  end
+
 end
